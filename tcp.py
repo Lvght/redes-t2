1fa96ea45ff2c3ec47c759c36e220f791c87280b
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2022-02-23 11:26:51 -0300

    Squashed commit of the following:
    commit bc2f9cf68e6b66ad72c3250ee41b13e854bac2e5
    Author: Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
    Date:   Wed Feb 23 11:23:44 2022 -0300
    
        Passando na E2
    
        Co-authored-by: NataliaBachiega <nataliabachiega@hotmail.com>
        Co-authored-by: Sfrev <igormachago@gmail.com>
        Co-authored-by: RafaelPassador <rafaelpassador1999@gmail.com>
    
    commit 9edd627e4e042f79cb6a08655e5dd12117772107
    Author: Vinicius da Luz <viniciusquaresma1904@gmail.com>
    Date:   Wed Feb 23 11:21:29 2022 -0300
    
        Passando na E2
    
        Co-authored-by: NataliaBachiega <nataliabachiega@hotmail.com>
        Co-authored-by: Sfrev <igormachago@gmail.com>
        Co-authored-by: RafaelPassador <rafaelpassador1999@gmail.com>
    
    commit 495a09c37b459fdc88ff4a109ae07ef61eda433e
    Author: Vinicius da Luz <viniciusquaresma1904@gmail.com>
    Date:   Wed Feb 23 11:09:59 2022 -0300
    
        Progresso na E2
    
        Co-authored-by: NataliaBachiega <nataliabachiega@hotmail.com>
        Co-authored-by: Sfrev <igormachago@gmail.com>
        Co-authored-by: RafaelPassador <rafaelpassador1999@gmail.com>
    
    commit cad3601555a1d4375c19ffac5cc620acb335c1c2
    Author: Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
    Date:   Thu Feb 17 16:13:00 2022 -0300
    
        Progresso na E2
    
        Co-authored-by: NataliaBachiega <nataliabachiega@hotmail.com>
        Co-authored-by: Sfrev <igormachago@gmail.com>
        Co-authored-by: RafaelPassador <rafaelpassador1999@gmail.com>

--- tcp.py
+++ tcp.py
@@ -1,8 +1,30 @@
 import asyncio
+
+import tcp
 from grader.tcputils import *
 
 # Usado para gerar bytes aleatórios
 import os
+
+
+class IdentificadorConexao:
+    porta_destino: int
+    porta_origem: int
+    endereco_destino: str
+    endereco_origem: str
+
+    def hash(self):
+        # Cria uma hash estática para o objeto
+        return hash(self.porta_destino) \
+               ^ hash(self.porta_origem) \
+               ^ hash(self.endereco_destino) \
+               ^ hash(self.endereco_origem)
+
+    def __init__(self, id_conexao: tuple):
+        self.porta_destino = id_conexao[3]
+        self.porta_origem = id_conexao[1]
+        self.endereco_destino = id_conexao[2]
+        self.endereco_origem = id_conexao[0]
 
 
 class Servidor:
@@ -15,81 +37,160 @@ class Servidor:
 
     def registrar_monitor_de_conexoes_aceitas(self, callback):
         """
-        Usado pela camada de aplicação para registrar uma função para ser chamada
-        sempre que uma nova conexão for aceita
+        Usado pela camada de aplicação para registrar uma função para ser
+        chamada sempre que uma nova conexão for aceita
         """
         self.callback = callback
 
     def _rdt_rcv(self, src_addr, dst_addr, segment):
         src_port, dst_port, seq_no, ack_no, \
-            flags, window_size, checksum, urg_ptr = read_header(segment)
+        flags, window_size, checksum, urg_ptr = read_header(segment)
 
         if dst_port != self.porta:
             # Ignora segmentos que não são destinados à porta do nosso servidor
             return
-        if not self.rede.ignore_checksum and calc_checksum(segment, src_addr, dst_addr) != 0:
+        if not self.rede.ignore_checksum and calc_checksum(segment, src_addr,
+                                                           dst_addr) != 0:
             print('descartando segmento com checksum incorreto')
             return
 
-        payload = segment[4*(flags>>12):]
-        id_conexao = (src_addr, src_port, dst_addr, dst_port)
+        payload = segment[4 * (flags >> 12):]
+
+        # Obtém os dados da conexão.
+        id_conexao = IdentificadorConexao(
+            (src_addr, src_port, dst_addr, dst_port))
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
-            # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
-            # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
-            # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
+            # A flag SYN estar setada significa que é um cliente tentando
+            # estabelecer uma conexão nova
+
+            # TODO: talvez você precise passar mais coisas para o
+            #  construtor de conexão
+
+            conexao = self.conexoes[id_conexao.hash()] = \
+                Conexao(self, id_conexao, sequence_number=seq_no)
+
+            # Registra a conexão no callback caso este não seja nulo.
+            if self.callback:
+                self.callback(conexao)
+
+            # TODO: você precisa fazer o handshake aceitando a conexão.
+            #  Escolha se você acha melhor
             # fazer aqui mesmo ou dentro da classe Conexao.
 
             ack_no: int = seq_no + 1
 
+            # Prepara a resposta do servidor.
             response = fix_checksum(
                 make_header(
-                    src_port=conexao.id_conexao[3],
-                    dst_port=conexao.id_conexao[1],
+                    src_port=conexao.id_conexao.porta_destino,
+                    dst_port=conexao.id_conexao.porta_origem,
                     seq_no=seq_no,
                     ack_no=ack_no,
                     flags=FLAGS_SYN | FLAGS_ACK
                 ),
-                dst_addr=conexao.id_conexao[2],
-                src_addr=conexao.id_conexao[0]
+                dst_addr=conexao.id_conexao.endereco_origem,
+                src_addr=conexao.id_conexao.endereco_destino
             )
 
-            conexao.enviar(response)
+            conexao.enviar(response, )
 
-            if self.callback:
+            if self.callback and not id_conexao.hash() in self.conexoes.keys():
                 self.callback(conexao)
-        elif id_conexao in self.conexoes:
+
+        elif id_conexao.hash() in self.conexoes.keys():
             # Passa para a conexão adequada se ela já estiver estabelecida
-            self.conexoes[id_conexao]._rdt_rcv(seq_no, ack_no, flags, payload)
+
+            conexao: tcp.Conexao = self.conexoes[id_conexao.hash()]
+
+            conexao._rdt_rcv(seq_no, ack_no, flags, payload)
         else:
             print('%s:%d -> %s:%d (pacote associado a conexão desconhecida)' %
                   (src_addr, src_port, dst_addr, dst_port))
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    sequence_number: int
+    acknowledge_number: int
+    id_conexao: IdentificadorConexao
+
+    def __init__(self, servidor, id_conexao, sequence_number: int):
         self.servidor = servidor
         self.id_conexao = id_conexao
         self.callback = None
-        self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
-        #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
+
+        # Usado para identificar a conexão.
+        self.sequence_number = sequence_number
+        self.acknowledge_number = sequence_number + 1
+
+        self.timer = asyncio.get_event_loop().call_later(1,
+                                                         self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
+
+        # self.timer.cancel()   # é possível cancelar o timer chamando esse
+        # método; esta linha é só um exemplo e pode ser removida
 
     def _exemplo_timer(self):
         # Esta função é só um exemplo e pode ser removida
         print('Este é um exemplo de como fazer um timer')
 
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
-        # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
-        # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
-        # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
-        print('recebido payload: %r' % payload)
+        # TODO: trate aqui o recebimento de segmentos provenientes da camada
+        #  de rede.
+        # Chame self.callback(self, dados) para passar dados para a camada
+        # de aplicação após
+        # garantir que eles não sejam duplicados e que tenham sido recebidos
+        # em ordem.
+
+        if seq_no != self.acknowledge_number:
+            return
+
+        if flags & FLAGS_FIN == FLAGS_FIN:
+            self.acknowledge_number += 1
+
+            # Envia a confirmação de recebimento com a flag ACK.
+
+            dados = fix_checksum(
+                make_header(
+                    src_port=self.id_conexao.porta_destino,
+                    dst_port=self.id_conexao.porta_origem,
+                    seq_no=seq_no,
+                    ack_no=ack_no,
+                    flags=FLAGS_ACK
+                ),
+                dst_addr=self.id_conexao.endereco_destino,
+                src_addr=self.id_conexao.endereco_origem
+            )
+
+            self.servidor.rede.enviar(dados,
+                                      self.id_conexao.endereco_origem)
+            self.callback(self, b'')
+
+        self.callback(self, payload)
+        if len(payload) != 0:
+            self.acknowledge_number += len(payload)
+
+            dados = fix_checksum(
+                make_header(
+                    src_port=self.id_conexao.porta_destino,
+                    dst_port=self.id_conexao.porta_origem,
+                    seq_no=self.sequence_number,
+                    ack_no=self.acknowledge_number,
+                    flags=flags | FLAGS_ACK
+                ),
+                dst_addr=self.id_conexao.endereco_origem,
+                src_addr=self.id_conexao.endereco_destino
+            )
+
+            self.servidor.rede.enviar(dados,
+                                      self.id_conexao.endereco_origem)
+            # print('recebido payload: %r' % payload)
 
     # Os métodos abaixo fazem parte da API
 
     def registrar_recebedor(self, callback):
         """
-        Usado pela camada de aplicação para registrar uma função para ser chamada
+        Usado pela camada de aplicação para registrar uma função para ser
+        chamada
         sempre que dados forem corretamente recebidos
         """
         self.callback = callback
@@ -99,10 +200,11 @@ class Conexao:
         Usado pela camada de aplicação para enviar dados
         """
         # TODO: implemente aqui o envio de dados.
-        # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
+        # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o
+        # segmento
         # que você construir para a camada de rede.
 
-        self.servidor.rede.enviar(dados, self.id_conexao[0])
+        self.servidor.rede.enviar(dados, self.id_conexao.endereco_origem)
 
     def fechar(self):
         """
